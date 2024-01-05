ce604839d7b9d83d213f1a8280f035e270560734
rmariano34 <ronna.mariano@gmail.com>
2024-01-05 13:53:35 -0600

    Loose and strict comparison

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,13 @@
-let ninjas = ['shaun', 'ryu', 'chun-li'];
+let age = 25;
 
-//ninjas[1] = 'ken';
-//console.log(ninjas[1]);
+//console.long(age, age + 3, `the age is ${age}`);
+// console.log(age == 25);
 
-//let ages = [20, 25, 30, 35];
-//console.log(ages[2]);
+//console.log(age != '25');
+//console.log(age != '25');
 
-//let random = ['shaun', 'crystal', 30, 20];
-//console.log(random);
-
-//console.log(ninjas.length);
-
-// array methods
-//let result = ninjas.join(',');
-//let result = ninjas.indexOf('chun-li');
-//let result = ninjas.concat('ken', 'crystal');
-let result = ninjas.push('ken');
-result = ninjas.pop();
-
-console.log(result);
+// strict comparison
+console.log(age === 25);
+console.log(age === '25');
+console.log(age !== 25);
+console.log(age !== '25');
