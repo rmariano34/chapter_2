7e2a58bbc35aaa219d594702617a121797bd75cb
rmariano34 <ronna.mariano@gmail.com>
2024-01-05 13:29:46 -0600

    Arrays

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,21 @@
-// template strings
-const title = 'Best reads of 2019';
-const author = 'Mario';
-const likes = 30; 
+let ninjas = ['shaun', 'ryu', 'chun-li'];
 
-// concatentation way
-//let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+//ninjas[1] = 'ken';
+//console.log(ninjas[1]);
 
-// template string way
-let result = `The blog called ${title} by ${author} has ${likes} likes`;
+//let ages = [20, 25, 30, 35];
+//console.log(ages[2]);
+
+//let random = ['shaun', 'crystal', 30, 20];
+//console.log(random);
+
+//console.log(ninjas.length);
+
+// array methods
+//let result = ninjas.join(',');
+//let result = ninjas.indexOf('chun-li');
+//let result = ninjas.concat('ken', 'crystal');
+let result = ninjas.push('ken');
+result = ninjas.pop();
+
 console.log(result);
-
-// creating html templates
-let html = `
-    <h2>${title}</h2>
-    <p>By ${author}</p>
-    <span>This blog has ${likes} likes</span>
-    `;
-console.log(html);
