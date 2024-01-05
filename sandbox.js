42288f4b7ca378658bca33459d44054c9e6a159b
rmariano34 <ronna.mariano@gmail.com>
2024-01-05 11:03:46 -0600

    Numbers - shorthand

--- sandbox.js
+++ sandbox.js
@@ -8,6 +8,26 @@ const pi = 3.14;
 //consol.log(10/2);
 
 //let result- radisu % 3;
-let result = pi * radius**2;
+//let result = pi * radius**2;
 
-console.log(result);
+// order of operation - BIDMAS
+
+//let result = 5 * (10=3)**2;
+
+//console.log(result);
+
+let likes = 10;
+
+//likes = likes + 1;
+//likes++;
+
+// Shorthand notation
+//likes += 10;
+
+//likes -= 5;
+
+//likes *= 2;
+
+//likes /= 2;
+
+console.log(likes);
